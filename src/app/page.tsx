23a748993cc058eb8ab2fa30ab7d9409deb1e50b
Saibal Ganguly <tech@aimindia.org.in>
2025-07-17 13:30:48 +0000

    Please revert. it will be totally different tahan home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import AboutUs from "@/components/sections/AboutUs";
 import MissionVision from "@/components/sections/MissionVision";
-import Team from "@/components/sections/Team";
+import DonateSection from "@/components/sections/DonateSection";
 
-export default function OrganisationPage() {
+export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
         <AboutUs />
         <MissionVision />
-        <Team />
+        <DonateSection />
       </main>
       <Footer />
     </div>
