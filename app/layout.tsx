7d71ba623b2954ad624e917fe40fda567e0b2761
Saibal Ganguly <tech@aimindia.org.in>
2025-07-17 06:35:02 +0000

    Move layout.tsx and page.tsx up one level. Move the files from: /app/src

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import './src/app/globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
