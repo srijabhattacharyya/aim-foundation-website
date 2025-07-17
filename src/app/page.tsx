3f7dc1c6cf03cc25ab4f713bfcface0c6e6bee6e
Saibal Ganguly <tech@aimindia.org.in>
2025-07-17 13:47:22 +0000

    You have changed the home page totally. Please bring it back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import Hero from "@/components/sections/Hero";
+import InfoSection from "@/components/sections/InfoSection";
 import AboutUs from "@/components/sections/AboutUs";
 import MissionVision from "@/components/sections/MissionVision";
-import DonateSection from "@/components/sections/DonateSection";
+import Impact from "@/components/sections/Impact";
+import Activities from "@/components/sections/Activities";
+import Testimonials from "@/components/sections/Testimonials";
+import Team from "@/components/sections/Team";
+import Blog from "@/components/sections/Blog";
+import Volunteer from "@/components/sections/Volunteer";
+import Newsletter from "@/components/sections/Newsletter";
+import Patrons from "@/components/sections/Patrons";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
+        <Hero />
+        <InfoSection />
         <AboutUs />
         <MissionVision />
-        <DonateSection />
+        <Impact />
+        <Activities />
+        <Testimonials />
+        <Patrons />
+        <Team />
+        <Blog />
+        <Volunteer />
+        <Newsletter />
       </main>
       <Footer />
     </div>
