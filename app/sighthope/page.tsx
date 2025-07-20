7c0dd471aa5e96a28a6be3247fa6727b103e6ca9
Saibal Ganguly <tech@aimindia.org.in>
2025-07-20 02:26:02 +0000

    Replace banner description by "Bringing Light to Lives"

--- app/sighthope/page.tsx
+++ app/sighthope/page.tsx
@@ -26,7 +26,7 @@ export default function SightHopePage() {
                 SightHope
               </h1>
               <p className="mt-4 text-lg md:text-xl max-w-3xl animate-fade-in-up [text-shadow:0_2px_4px_rgba(0,0,0,0.5)]">
-                Bringing Vision to Life.
+                Bringing Light to Lives
               </p>
             </div>
           </div>
