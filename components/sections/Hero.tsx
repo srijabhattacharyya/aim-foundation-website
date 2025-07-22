b207ef358abdf3e6dde4b507922013868785b232
Saibal Ganguly <tech@aimindia.org.in>
2025-07-22 01:16:23 +0000

    Will you check this?: export default function Hero() {   return (     <s

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -69,7 +69,7 @@ const programs = [
 
 const Hero = () => {
   return (
-    <section className="relative w-screen h-screen flex flex-col -ml-[calc(50vw-50%)]">
+    <section className="relative w-screen h-screen flex flex-col left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]">
       <div className="absolute inset-0">
         <picture className="w-full h-full block">
             <source srcSet="/images/banner/home.avif" type="image/avif" />
