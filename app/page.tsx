ee11fd06b0480cfa504873b5c5b6c9f911a8ba5a
Saibal Ganguly <tech@aimindia.org.in>
2025-07-25 13:26:01 +0000

    Reduce to some extent but not a great extent

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import Hero from "../components/sections/Hero";
+import InfoSection from '../components/sections/InfoSection';
+import AboutUs from '../components/sections/AboutUs';
+import Impact from '../components/sections/Impact';
+import Activities from '../components/sections/Activities';
+import DonateSection from '../components/sections/DonateSection';
 import dynamic from 'next/dynamic';
 
-const InfoSection = dynamic(() => import('../components/sections/InfoSection'));
-const AboutUs = dynamic(() => import('../components/sections/AboutUs'));
-const Impact = dynamic(() => import('../components/sections/Impact'));
-const Activities = dynamic(() => import('../components/sections/Activities'));
-const DonateSection = dynamic(() => import('../components/sections/DonateSection'));
 const Testimonials = dynamic(() => import('../components/sections/Testimonials'));
 const Patrons = dynamic(() => import('../components/sections/Patrons'));
 const Blog = dynamic(() => import('../components/sections/Blog'));
