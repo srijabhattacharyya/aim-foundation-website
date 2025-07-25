b5249c371d428997ec68fae93a2814e93cb3a4f1
Saibal Ganguly <tech@aimindia.org.in>
2025-07-25 15:36:25 +0000

    Keep Bricolage_Grotesque no problem. Keep it as it was

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-inter)', 'sans-serif'],
+        body: ['var(--font-bricolage)', 'sans-serif'],
+        headline: ['var(--font-bricolage)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
