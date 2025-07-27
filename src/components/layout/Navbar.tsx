2e5549549773757b6ba126236e081c7583e252cd
Saibal Ganguly <tech@aimindia.org.in>
2025-07-27 07:38:36 +0000

    There is huge blank space in the left side of the logo. This is not look

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -183,7 +183,7 @@ const Navbar = () => {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container mx-auto flex h-[100px] items-center justify-between px-4 md:px-6">
+        <div className="flex h-[100px] items-center justify-between px-4 md:px-6">
           <div className="flex h-full items-center">
             <Link href="/" className="flex items-center" aria-label="AIM Foundation Home">
               <Image src="/images/logo.png" alt="AIM Foundation Logo" width={150} height={60} />
