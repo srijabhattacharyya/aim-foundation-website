4d101ea1b90ae741e6dec502ce607922ecb89c54
Saibal Ganguly <tech@aimindia.org.in>
2025-08-04 03:06:48 +0000

    Do not use src/components/sections/HeroCarousel.tsx, Use /components/sec

--- components/sections/HeroCarousel.tsx
+++ components/sections/HeroCarousel.tsx
@@ -46,7 +46,7 @@ const programs = [
   {
     title: 'Relief to the underprivileged',
     description: 'Offering support and resources to those in need.',
-    image: 'https://placehold.co/160x160.png',
+    image: '/images/banner-card/relief.png',
     hint: 'underprivileged support',
     link: '/relief-to-the-underprivileged',
   },
