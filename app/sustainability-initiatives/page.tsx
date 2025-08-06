42e9f9bb075742d4e764b41d7df8ab91269b0411
Saibal Ganguly <tech@aimindia.org.in>
2025-08-06 08:33:48 +0000

    In /sustainability-initiatives page insert /images/banner/health.jpg as

--- app/sustainability-initiatives/page.tsx
+++ app/sustainability-initiatives/page.tsx
@@ -38,7 +38,7 @@ export default function SustainabilityInitiativesPage() {
         <section className="relative w-full">
           <div className="relative w-full">
             <Image
-              src="https://placehold.co/1920x580.png"
+              src="/images/banner/health.jpg"
               alt="Sustainability Initiatives Banner"
               width={1920}
               height={580}
