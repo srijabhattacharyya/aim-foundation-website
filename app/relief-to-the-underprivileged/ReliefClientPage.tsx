dc23d6faf73b63ddff7931f7a24d9444168d1b54
Saibal Ganguly <tech@aimindia.org.in>
2025-08-06 08:39:21 +0000

    In /relief-to-the-underprivileged page insert /images/banner/relief.jpg

--- app/relief-to-the-underprivileged/ReliefClientPage.tsx
+++ app/relief-to-the-underprivileged/ReliefClientPage.tsx
@@ -37,7 +37,7 @@ export default function ReliefClientPage() {
         <section className="relative w-full">
           <div className="relative w-full">
             <Image
-              src="https://placehold.co/1920x580.png"
+              src="/images/banner/relief.jpg"
               alt="Relief for the Underprivileged Banner"
               width={1920}
               height={580}
