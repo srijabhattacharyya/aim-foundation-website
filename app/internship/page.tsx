ad91a5299e45b00433ca8a293f5d673322e281a5
Saibal Ganguly <tech@aimindia.org.in>
2025-08-06 10:38:09 +0000

    In /internship page insert /images/banner/intern.jpg as /internship page

--- app/internship/page.tsx
+++ app/internship/page.tsx
@@ -14,7 +14,7 @@ export default function InternshipPage() {
         <section className="relative w-full">
           <div className="relative w-full">
             <Image
-              src="https://placehold.co/1920x580.png"
+              src="/images/banner/intern.jpg"
               alt="Internship Banner"
               width={1920}
               height={580}
