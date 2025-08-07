2798723ea86e258f20e7a7fb8853b23f2ce37a22
Saibal Ganguly <tech@aimindia.org.in>
2025-08-07 03:05:40 +0000

    Kindly make description in /suidhaga page donation form all Capitals

--- components/sections/donation-forms/SuiDhagaDonationForm.tsx
+++ components/sections/donation-forms/SuiDhagaDonationForm.tsx
@@ -63,17 +63,17 @@ const donationSchema = z.object({
 });
 
 const donationAmountsIndian = [
-    { value: "3500", label: "₹3500", description: "Basic tailoring training to 1 woman" },
-    { value: "7000", label: "₹7000", description: "Advance tailoring training for 1 woman" },
-    { value: "14000", label: "₹14000", description: "SPONSOR A SEWING MACHINE & Basic Tailoring TRAINING" },
-    { value: "21000", label: "₹21000", description: "SPONSOR A SEWING MACHINE & Complete TRAINING" },
+    { value: "3500", label: "₹3500", description: "BASIC TAILORING TRAINING TO 1 WOMAN" },
+    { value: "7000", label: "₹7000", description: "ADVANCE TAILORING TRAINING FOR 1 WOMAN" },
+    { value: "14000", label: "₹14000", description: "SPONSOR A SEWING MACHINE & BASIC TAILORING TRAINING" },
+    { value: "21000", label: "₹21000", description: "SPONSOR A SEWING MACHINE & COMPLETE TRAINING" },
 ];
 
 const donationAmountsNonIndian = [
-    { value: "42", label: "$42", description: "Basic tailoring training to 1 woman" },
-    { value: "84", label: "$84", description: "Advance tailoring training for 1 woman" },
-    { value: "168", label: "$168", description: "SPONSOR A SEWING MACHINE & Basic Tailoring TRAINING" },
-    { value: "252", label: "$252", description: "SPONSOR A SEWING MACHINE & Complete TRAINING" },
+    { value: "42", label: "$42", description: "BASIC TAILORING TRAINING TO 1 WOMAN" },
+    { value: "84", label: "$84", description: "ADVANCE TAILORING TRAINING FOR 1 WOMAN" },
+    { value: "168", label: "$168", description: "SPONSOR A SEWING MACHINE & BASIC TAILORING TRAINING" },
+    { value: "252", label: "$252", description: "SPONSOR A SEWING MACHINE & COMPLETE TRAINING" },
 ];
 
 export default function SuiDhagaDonationForm() {
