a248aa80bd5091d841cd04ba5b498ee4a5652ed8
Saibal Ganguly <tech@aimindia.org.in>
2025-08-07 02:38:55 +0000

    in /cyclesafe SUPPORT A MENSTRUAL HYGIENE WORKSHOP make Rs. 10000

--- components/sections/donation-forms/CycleSafeDonationForm.tsx
+++ components/sections/donation-forms/CycleSafeDonationForm.tsx
@@ -66,14 +66,14 @@ const donationAmountsIndian = [
     { value: "500", label: "₹500", description: "SANITARY NAPKINS FOR A GIRL FOR A YEAR" },
     { value: "1000", label: "₹1000", description: "SANITARY NAPKINS FOR 2 GIRLS FOR A YEAR" },
     { value: "2500", label: "₹2500", description: "SANITARY NAPKINS FOR 5 GIRLS FOR A YEAR" },
-    { value: "5000", label: "₹5000", description: "SUPPORT A MENSTRUAL HYGIENE WORKSHOP" },
+    { value: "10000", label: "₹10000", description: "SUPPORT A MENSTRUAL HYGIENE WORKSHOP" },
 ];
 
 const donationAmountsNonIndian = [
     { value: "6", label: "$6", description: "SANITARY NAPKINS FOR A GIRL FOR A YEAR" },
     { value: "12", label: "$12", description: "SANITARY NAPKINS FOR 2 GIRLS FOR A YEAR" },
     { value: "30", label: "$30", description: "SANITARY NAPKINS FOR 5 GIRLS FOR A YEAR" },
-    { value: "60", label: "$60", description: "SUPPORT A MENSTRUAL HYGIENE WORKSHOP" },
+    { value: "120", label: "$120", description: "SUPPORT A MENSTRUAL HYGIENE WORKSHOP" },
 ];
 
 export default function CycleSafeDonationForm() {
