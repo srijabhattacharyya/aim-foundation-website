d44dcd6a71d941643910631406342f8e2b8aa8b4
Saibal Ganguly <tech@aimindia.org.in>
2025-08-07 01:58:59 +0000

    In inspire-eduLab FULL EDULAB SETUP cost is  Rs. 100000

--- components/sections/donation-forms/InspireEduLabDonationForm.tsx
+++ components/sections/donation-forms/InspireEduLabDonationForm.tsx
@@ -66,14 +66,14 @@ const donationAmountsIndian = [
     { value: "3000", label: "₹3000", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR 3 MONTHS" },
     { value: "6000", label: "₹6000", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR 6 MONTHS" },
     { value: "12000", label: "₹12000", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR A YEAR" },
-    { value: "24000", label: "₹24000", description: "SPONSOR A FULL EDULAB SETUP" },
+    { value: "100000", label: "₹100000", description: "SPONSOR A FULL EDULAB SETUP" },
 ];
 
 const donationAmountsNonIndian = [
     { value: "35", label: "$35", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR 3 MONTHS" },
     { value: "70", label: "$70", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR 6 MONTHS" },
     { value: "140", label: "$140", description: "DIGITAL TOOLS FOR 1 CLASSROOM FOR A YEAR" },
-    { value: "280", label: "$280", description: "SPONSOR A FULL EDULAB SETUP" },
+    { value: "1200", label: "$1200", description: "SPONSOR A FULL EDULAB SETUP" },
 ];
 
 export default function InspireEduLabDonationForm() {
