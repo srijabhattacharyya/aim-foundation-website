68ed28e06d671fad4b7d35b3a7b3415987b8c0c2
Saibal Ganguly <tech@aimindia.org.in>
2025-08-07 02:27:40 +0000

    Make 30000 for one awarness camp in place of 15000 for 10 screening

--- components/sections/donation-forms/DetectDonationForm.tsx
+++ components/sections/donation-forms/DetectDonationForm.tsx
@@ -66,14 +66,14 @@ const donationAmountsIndian = [
     { value: "1500", label: "₹1500", description: "SPONSOR SCREENING FOR 1 WOMAN" },
     { value: "3000", label: "₹3000", description: "SPONSOR SCREENING FOR 2 WOMEN" },
     { value: "7500", label: "₹7500", description: "SPONSOR SCREENING FOR 5 WOMEN" },
-    { value: "15000", label: "₹15000", description: "SPONSOR SCREENING FOR 10 WOMEN" },
+    { value: "30000", label: "₹30000", description: "SPONSOR AN AWARENESS CAMP" },
 ];
 
 const donationAmountsNonIndian = [
     { value: "18", label: "$18", description: "SPONSOR SCREENING FOR 1 WOMAN" },
     { value: "36", label: "$36", description: "SPONSOR SCREENING FOR 2 WOMEN" },
     { value: "90", label: "$90", description: "SPONSOR SCREENING FOR 5 WOMEN" },
-    { value: "180", label: "$180", description: "SPONSOR SCREENING FOR 10 WOMEN" },
+    { value: "360", label: "$360", description: "SPONSOR AN AWARENESS CAMP" },
 ];
 
 export default function DetectDonationForm() {
