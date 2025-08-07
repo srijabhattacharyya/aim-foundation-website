a84b0899509b2980d68ab6fb1dbc870b25c08d89
Saibal Ganguly <tech@aimindia.org.in>
2025-08-07 02:22:11 +0000

    in /childfirst SPONSOR A HEALTH CAMP FOR CHILDREN shall metion that with

--- components/sections/donation-forms/ChildFirstDonationForm.tsx
+++ components/sections/donation-forms/ChildFirstDonationForm.tsx
@@ -66,14 +66,14 @@ const donationAmountsIndian = [
     { value: "2500", label: "₹2500", description: "HEALTH & NUTRITION FOR ONE CHILD" },
     { value: "5000", label: "₹5000", description: "HEALTH & NUTRITION FOR TWO CHILDREN" },
     { value: "10000", label: "₹10000", description: "HEALTH & NUTRITION FOR FOUR CHILDREN" },
-    { value: "20000", label: "₹20000", description: "SPONSOR A HEALTH CAMP FOR CHILDREN" },
+    { value: "20000", label: "₹20000", description: "SPONSOR A HEALTH CAMP FOR CHILDREN (EXCLUDING MEDICINE)" },
 ];
 
 const donationAmountsNonIndian = [
     { value: "30", label: "$30", description: "HEALTH & NUTRITION FOR ONE CHILD" },
     { value: "60", label: "$60", description: "HEALTH & NUTRITION FOR TWO CHILDREN" },
     { value: "120", label: "$120", description: "HEALTH & NUTRITION FOR FOUR CHILDREN" },
-    { value: "240", label: "$240", description: "SPONSOR A HEALTH CAMP FOR CHILDREN" },
+    { value: "240", label: "$240", description: "SPONSOR A HEALTH CAMP FOR CHILDREN (EXCLUDING MEDICINE)" },
 ];
 
 
