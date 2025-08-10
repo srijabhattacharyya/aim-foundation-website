2c130f5ac8d1780a5aa42499652559b6f7794e90
Saibal Ganguly <tech@aimindia.org.in>
2025-08-10 16:35:39 +0000

    Database finally created in https://console.firebase.google.com/u/0/proj

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,17 +8,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  authDomain: "aim-foundation-website.firebaseapp.com",
+  projectId: "aim-foundation-website",
+  storageBucket: "aim-foundation-website.appspot.com",
+  messagingSenderId: "369069818961",
+  appId: "1:369069818961:web:757e7424c581373950f0c0",
+  measurementId: "G-G6J36E91R3"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+const db = getFirestore(app, 'aim-foundation-website');
 const storage = getStorage(app);
 const auth = getAuth(app);
 
