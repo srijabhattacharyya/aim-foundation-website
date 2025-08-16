47cd63eb7ad83d841f79350e7ff7ebe910523599
Saibal Ganguly <tech@aimindia.org.in>
2025-08-16 11:30:08 +0000

    I want to make the banner place holder for /milieu page 1920 x 580

--- app/milieu/MilieuClientPage.tsx
+++ app/milieu/MilieuClientPage.tsx
@@ -12,7 +12,7 @@ import { Dialog, DialogContent, DialogTrigger } from "../../components/ui/dialog
 import InitiativeSidebar from "@/components/layout/InitiativeSidebar";
 import { useSearchParams } from 'next/navigation';
 
-const DynamicDonationForm = dynamic(() => import('../../components/sections/donation-forms/MilieuDonationForm'), { 
+const DynamicDonationForm = dynamic(() => import('@/components/sections/donation-forms/MilieuDonationForm'), { 
     ssr: false,
     loading: () => <div className="p-8"><Skeleton className="h-[500px] w-full" /></div> 
 });
@@ -29,10 +29,10 @@ export default function MilieuClientPage() {
             <section className="relative w-full">
               <div className="relative w-full">
                 <Image
-                  src="/images/banner/milieu.avif"
+                  src="https://placehold.co/1920x580.png"
                   alt="Milieu Banner"
                   width={1920}
-                  height={334}
+                  height={580}
                   className="w-full h-auto object-cover"
                   data-ai-hint="diverse children interacting"
                 />
