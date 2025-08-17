94a0672ede324d61c5dc7fe81c2697c629b2d831
Saibal Ganguly <tech@aimindia.org.in>
2025-08-17 06:11:45 +0000

    I do not consider server side rendering of donation from etc. was the pr

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import { Bricolage_Grotesque } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Chatbot from '@/components/chatbot/Chatbot';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.aimindia.org.in'),
@@ -120,7 +119,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Chatbot />
         <Toaster />
       </body>
     </html>
