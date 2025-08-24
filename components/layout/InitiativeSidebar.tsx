0659a08df69712753c0619b65522c077550fa918
Saibal Ganguly <tech@aimindia.org.in>
2025-08-24 06:15:17 +0000

    In /roots-of-change page use /images/projects/sustainability/sustainabil

--- components/layout/InitiativeSidebar.tsx
+++ components/layout/InitiativeSidebar.tsx
@@ -210,7 +210,7 @@ export default function InitiativeSidebar({ from }: InitiativeSidebarProps) {
                     Roots of Change: Planting Awareness, Growing a Greener Tomorrow
                 </Link>
             );
-            image = { src: "/images/banner/roots-of-change1.avif", alt: "Roots of Change initiative", hint: "environmental awareness education", description: "An interactive session on environmental conservation"};
+            image = { src: "/images/projects/sustainability/sustainability3.avif", alt: "Roots of Change initiative", hint: "environmental education", description: "An interactive session on environmental conservation"};
         } else if (pathname.includes('/sheconnects')) {
             content = (
                 <Link href="/blog/project-sheconnects" className="text-muted-foreground hover:text-primary transition-colors">
