0b3fc79f74e56042caaf91e70c59e7c854060518
Saibal Ganguly <tech@aimindia.org.in>
2025-08-24 06:18:25 +0000

    In /roots-of-change page insert banner /images/banner/roots-of-change1.a

--- app/roots-of-change/RootsOfChangeClientPage.tsx
+++ app/roots-of-change/RootsOfChangeClientPage.tsx
@@ -26,7 +26,7 @@ export default function RootsOfChangeClientPage() {
         <section className="relative w-full">
           <div className="relative w-full">
             <Image
-              src="/images/banner/roots-of-change.avif"
+              src="/images/banner/roots-of-change1.avif"
               alt="Roots of Change Banner"
               width={1920}
               height={580}
@@ -35,7 +35,7 @@ export default function RootsOfChangeClientPage() {
               priority
             />
           </div>
-          <div className="absolute inset-0 flex items-end justify-start text-white p-8 md:p-12">
+          <div className="absolute inset-0 flex items-end justify-start bg-gradient-to-t from-black/30 to-transparent text-white p-8 md:p-12">
             <div className="relative z-10 text-left">
               <h1 className="text-4xl md:text-5xl font-bold font-headline animate-fade-in-down [text-shadow:0_2px_4px_rgba(0,0,0,0.5)]">
                 Roots of Change
