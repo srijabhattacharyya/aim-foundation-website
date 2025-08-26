d0d93dee0840c0a82a8dab1dfa743e5e3d9c726c
Saibal Ganguly <tech@aimindia.org.in>
2025-08-26 06:24:52 +0000

    Please check

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
   },
   experimental: {
