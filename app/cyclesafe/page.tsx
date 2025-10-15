c0341a7647b18e2fce6d421af9762f4c58c0fc31
Saibal Ganguly <tech@aimindia.org.in>
2025-10-15 13:56:31 +0000

    Fix detect page: add client directive for useSearchParams support

--- app/cyclesafe/page.tsx
+++ app/cyclesafe/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from 'next';
 import CycleSafeClientPage from './CycleSafeClientPage';
 
--- app/detect/page.tsx
+++ app/detect/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from 'next';
 import DetectClientPage from './DetectClientPage';
 
