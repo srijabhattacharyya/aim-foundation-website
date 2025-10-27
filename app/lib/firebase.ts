b4ad568a2ef155847568592520d9f14715a698e4
Saibal Ganguly <tech@aimindia.org.in>
2025-10-27 08:31:06 +0000

    Debabse Connection Error Fix

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,7 +4,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Make sure you have these environment variables set in Vercel
+// Make sure you have these environment variables set in Vercel properly
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
