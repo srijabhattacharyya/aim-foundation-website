d5a7602ec5ed32c2fdb52f753972125761dff676
Saibal Ganguly <tech@aimindia.org.in>
2025-10-27 05:12:52 +0000

    Update Your Firebase / Google SDK Initialization Code
    Wherever you init

--- src/app/lib/firebase-admin-config.ts
+++
@@ -1,8 +0,0 @@
-// src/app/lib/firebase-admin-config.ts
-// IMPORTANT: This file should be in your .gitignore file to prevent leaking secrets.
-
-export const firebaseAdminConfig = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-};
