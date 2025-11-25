7e7eb35260f7c58123741ac82fe29debc6c8eb9b
Saibal Ganguly <tech@aimindia.org.in>
2025-11-25 18:17:25 +0000

    Kindly add the folowing facebook pixel to every page: <!-- Meta Pixel Co

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,27 @@ export default function RootLayout({
             gtag('config', '${adwordsId}');
           `}
         </Script>
+         {/* Meta Pixel Code */}
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1912559689679432');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+          src="https://www.facebook.com/tr?id=1912559689679432&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className="font-body antialiased">
         {children}
