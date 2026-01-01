b81ed6a2d029b63d3b208988265c8188ab2397b6
Saibal Ganguly <tech@aimindia.org.in>
2026-01-01 05:41:48 +0000

    In donation for graphic design  please change to "SUPPORT GRAPHIC DESIGN

--- components/sections/donation-forms/GraphicDesignDonationForm.tsx
+++ components/sections/donation-forms/GraphicDesignDonationForm.tsx
@@ -24,7 +24,7 @@ export default function GraphicDesignDonationForm() {
             donationAmountsNonIndian={donationAmountsNonIndian}
             defaultIndianAmount="3000"
             defaultNonIndianAmount="36"
-            formTitle="SUPPORT GRAPHIC DESIGNING"
+            formTitle="SUPPORT GRAPHIC DESIGNING Training"
             formSubtitle="EMPOWER A CREATIVE FUTURE"
         />
     );
