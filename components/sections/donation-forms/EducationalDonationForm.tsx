ae220e9d82200bdb6738b1b95a146573070d4622
Saibal Ganguly <tech@aimindia.org.in>
2026-03-14 06:00:32 +0000

    In GENERAL DONATION FOR EDUCATIONAL INITIATIVES FORM Only keep radio but

--- components/sections/donation-forms/EducationalDonationForm.tsx
+++ components/sections/donation-forms/EducationalDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "3000", label: "₹3000", description: "SUPPORT A CHILD'S EDUCATION FOR 6 MONTHS" },
-    { value: "6000", label: "₹6000", description: "SUPPORT A CHILD'S EDUCATION FOR A YEAR" },
-    { value: "12000", label: "₹12000", description: "SUPPORT 2 CHILDREN'S EDUCATION FOR A YEAR" },
-    { value: "24000", label: "₹24000", description: "SPONSOR A LEARNING CENTER FOR A MONTH" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "35", label: "$35", description: "SUPPORT A CHILD'S EDUCATION FOR 6 MONTHS" },
-    { value: "70", label: "$70", description: "SUPPORT A CHILD'S EDUCATION FOR A YEAR" },
-    { value: "140", label: "$140", description: "SUPPORT 2 CHILDREN'S EDUCATION FOR A YEAR" },
-    { value: "280", label: "$280", description: "SPONSOR A LEARNING CENTER FOR A MONTH" },
-];
-
 export default function EducationalDonationForm() {
     return (
         <DonationForm
-            cause="Educational"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="3000"
-            defaultNonIndianAmount="35"
-            formTitle="SUPPORT EDUCATIONAL INITIATIVES"
-            formSubtitle="MAKE A DIFFERENCE"
+            cause="Educational Initiatives"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
+            formTitle="SUPPORT OUR EDUCATIONAL INITIATIVES"
+            formSubtitle="YOUR CONTRIBUTION MATTERS"
+            hideAmount={true}
+            razorpayButtonId="pl_SQzwgo48pje7Ru"
         />
     );
 }
