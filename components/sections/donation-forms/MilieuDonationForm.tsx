f347dbf5fc0aa8315195a319eff201820f64695f
Saibal Ganguly <tech@aimindia.org.in>
2026-03-14 07:28:38 +0000

    In MILIEU FORM Only keep radio button for Indian and non-Indian, No amou

--- components/sections/donation-forms/MilieuDonationForm.tsx
+++ components/sections/donation-forms/MilieuDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "2500", label: "₹2500", description: "SUPPORT A WORKSHOP FOR 5 CHILDREN" },
-    { value: "5000", label: "₹5000", description: "SUPPORT A WORKSHOP FOR 10 CHILDREN" },
-    { value: "10000", label: "₹10000", description: "SPONSOR LEARNING MATERIALS" },
-    { value: "20000", label: "₹20000", description: "SPONSOR A FULL MILIEU SESSION" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "30", label: "$30", description: "SUPPORT A WORKSHOP FOR 5 CHILDREN" },
-    { value: "60", label: "$60", description: "SUPPORT A WORKSHOP FOR 10 CHILDREN" },
-    { value: "120", label: "$120", description: "SPONSOR LEARNING MATERIALS" },
-    { value: "240", label: "$240", description: "SPONSOR A FULL MILIEU SESSION" },
-];
-
 export default function MilieuDonationForm() {
     return (
         <DonationForm
             cause="Milieu"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="2500"
-            defaultNonIndianAmount="30"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT MILIEU"
             formSubtitle="BUILDING BRIDGES, NURTURING EMPATHY"
+            hideAmount={true}
+            razorpayButtonId="pl_SR1TjSIzKZaGPy"
         />
     );
 }
