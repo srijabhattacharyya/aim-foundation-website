bc61b96e550eaf5013c30be7db524786d007e1c7
Saibal Ganguly <tech@aimindia.org.in>
2026-03-14 18:51:48 +0000

    In General Donation for Healthcare Initiative donation FORM Only keep ra

--- components/sections/donation-forms/HealthcareDonationForm.tsx
+++ components/sections/donation-forms/HealthcareDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "1500", label: "₹1500", description: "PROVIDE MEDICINES FOR 5 PEOPLE" },
-    { value: "3000", label: "₹3000", description: "SUPPORT A HEALTH CHECK-UP CAMP" },
-    { value: "5000", label: "₹5000", description: "SPONSOR A MINOR SURGERY" },
-    { value: "20000", label: "₹20000", description: "SUPPORT A FULL HEALTH CAMP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "18", label: "$18", description: "PROVIDE MEDICINES FOR 5 PEOPLE" },
-    { value: "36", label: "$36", description: "SUPPORT A HEALTH CHECK-UP CAMP" },
-    { value: "60", label: "$60", description: "SPONSOR A MINOR SURGERY" },
-    { value: "240", label: "$240", description: "SUPPORT A FULL HEALTH CAMP" },
-];
-
 export default function HealthcareDonationForm() {
     return (
         <DonationForm
             cause="Healthcare"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="1500"
-            defaultNonIndianAmount="18"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT HEALTHCARE INITIATIVES"
             formSubtitle="PROVIDE VITAL MEDICAL CARE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRD6isYNKqxDzH"
         />
     );
 }
