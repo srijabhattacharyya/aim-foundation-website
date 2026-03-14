1553ad58da09afd8a99e354875d9d988bb851276
Saibal Ganguly <tech@aimindia.org.in>
2026-03-14 07:06:34 +0000

    In EDUACCESS FORM Only keep radio button for Indian and non-Indian, No a

--- components/sections/donation-forms/EduAccessDonationForm.tsx
+++ components/sections/donation-forms/EduAccessDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "3000", label: "₹3000", description: "ONLINE CLASSES FOR 1 CHILD FOR 6 MONTHS" },
-    { value: "6000", label: "₹6000", description: "ONLINE CLASSES FOR 1 CHILD FOR A YEAR" },
-    { value: "12000", label: "₹12000", description: "ONLINE CLASSES FOR 2 CHILDREN FOR A YEAR" },
-    { value: "24000", label: "₹24000", description: "ONLINE CLASSES FOR 4 CHILDREN FOR A YEAR" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "35", label: "$35", description: "ONLINE CLASSES FOR 1 CHILD FOR 6 MONTHS" },
-    { value: "70", label: "$70", description: "ONLINE CLASSES FOR 1 CHILD FOR A YEAR" },
-    { value: "140", label: "$140", description: "ONLINE CLASSES FOR 2 CHILDREN FOR A YEAR" },
-    { value: "280", label: "$280", description: "ONLINE CLASSES FOR 4 CHILDREN FOR A YEAR" },
-];
-
 export default function EduAccessDonationForm() {
     return (
         <DonationForm
             cause="EduAccess"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="3000"
-            defaultNonIndianAmount="35"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT EDUACCESS"
             formSubtitle="MAKE A DIFFERENCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SR13XkTMxcm3AS"
         />
     );
 }
