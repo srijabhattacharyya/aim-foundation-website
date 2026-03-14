c9839942be42488df229aa5d3263b65e1c4228ff
Saibal Ganguly <tech@aimindia.org.in>
2026-03-14 07:34:30 +0000

    In VIDYASHAKTI FORM Only keep radio button for Indian and non-Indian, No

--- components/sections/donation-forms/VidyaShaktiDonationForm.tsx
+++ components/sections/donation-forms/VidyaShaktiDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "3000", label: "₹3000", description: "APP ACCESS FOR 10 CHILDREN FOR A YEAR" },
-    { value: "6000", label: "₹6000", description: "APP ACCESS FOR 20 CHILDREN FOR A YEAR" },
-    { value: "12000", label: "₹12000", description: "APP ACCESS FOR 40 CHILDREN FOR A YEAR" },
-    { value: "24000", label: "₹24000", description: "SUPPORT APP DEVELOPMENT & MAINTENANCE" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "35", label: "$35", description: "APP ACCESS FOR 10 CHILDREN FOR A YEAR" },
-    { value: "70", label: "$70", description: "APP ACCESS FOR 20 CHILDREN FOR A YEAR" },
-    { value: "140", label: "$140", description: "APP ACCESS FOR 40 CHILDREN FOR A YEAR" },
-    { value: "280", label: "$280", description: "SUPPORT APP DEVELOPMENT & MAINTENANCE" },
-];
-
 export default function VidyaShaktiDonationForm() {
     return (
         <DonationForm
             cause="VidyaShakti"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="3000"
-            defaultNonIndianAmount="35"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT VIDYASHAKTI"
             formSubtitle="MAKE A DIFFERENCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SR1Yr47xn26vaZ"
         />
     );
 }
