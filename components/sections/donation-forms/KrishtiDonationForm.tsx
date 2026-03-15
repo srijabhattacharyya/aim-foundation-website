cf10be193550bb6d8414fe94e154d615ed79a726
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 03:42:25 +0000

    In KRISHTI donation FORM Only keep radio button for Indian and non-India

--- components/sections/donation-forms/KrishtiDonationForm.tsx
+++ components/sections/donation-forms/KrishtiDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "2500", label: "₹2500", description: "SUPPORT A WOMAN'S ENTREPRENEURIAL JOURNEY" },
-    { value: "5000", label: "₹5000", description: "PROVIDE RAW MATERIALS FOR A SMALL BATCH" },
-    { value: "10000", label: "₹10000", description: "HELP WITH MARKETING & PACKAGING" },
-    { value: "20000", label: "₹20000", description: "SPONSOR A NEW PRODUCTION SETUP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "30", label: "$30", description: "SUPPORT A WOMAN'S ENTREPRENEURIAL JOURNEY" },
-    { value: "60", label: "$60", description: "PROVIDE RAW MATERIALS FOR A SMALL BATCH" },
-    { value: "120", label: "$120", description: "HELP WITH MARKETING & PACKAGING" },
-    { value: "240", label: "$240", description: "SPONSOR A NEW PRODUCTION SETUP" },
-];
-
 export default function KrishtiDonationForm() {
     return (
         <DonationForm
             cause="Krishti"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="2500"
-            defaultNonIndianAmount="30"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT KRISHTI"
             formSubtitle="FROM SKILL TO SELF-RELIANCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRMAIxinR59A5l"
         />
     );
 }
