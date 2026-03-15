401c636c2752bdbfe4f1f2131fbc9ec360903f9b
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 04:33:27 +0000

    In Support to underprivileged donation FORM Only keep radio button for I

--- components/sections/donation-forms/ReliefDonationForm.tsx
+++ components/sections/donation-forms/ReliefDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "1000", label: "₹1000", description: "PROVIDE AN ESSENTIALS KIT" },
-    { value: "2500", label: "₹2500", description: "PROVIDE FOOD FOR A FAMILY FOR A WEEK" },
-    { value: "5000", label: "₹5000", description: "SUPPORT A SMALL RELIEF CAMP" },
-    { value: "10000", label: "₹10000", description: "SUPPORT A COMPREHENSIVE RELIEF CAMP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "12", label: "$12", description: "PROVIDE AN ESSENTIALS KIT" },
-    { value: "30", label: "$30", description: "PROVIDE FOOD FOR A FAMILY FOR A WEEK" },
-    { value: "60", label: "$60", description: "SUPPORT A SMALL RELIEF CAMP" },
-    { value: "120", label: "$120", description: "SUPPORT A COMPREHENSIVE RELIEF CAMP" },
-];
-
 export default function ReliefDonationForm() {
     return (
         <DonationForm
             cause="Relief to the Underprivileged"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="1000"
-            defaultNonIndianAmount="12"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT RELIEF EFFORTS"
             formSubtitle="PROVIDE IMMEDIATE AID"
+            hideAmount={true}
+            razorpayButtonId="pl_SRN279VxAFLbge"
         />
     );
 }
