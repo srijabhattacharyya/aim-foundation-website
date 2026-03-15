c72f8788c4c305f239d49030fbe777581248c183
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 02:36:06 +0000

    In ORALSCAN donation FORM Only keep radio button for Indian and non-Indi

--- components/sections/donation-forms/OralScanDonationForm.tsx
+++ components/sections/donation-forms/OralScanDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "1000", label: "₹1000", description: "SPONSOR AN ORAL CANCER SCREENING KIT" },
-    { value: "2500", label: "₹2500", description: "SUPPORT AN AWARENESS SESSION" },
-    { value: "5000", label: "₹5000", description: "SUPPORT A SMALL SCREENING CAMP" },
-    { value: "10000", label: "₹10000", description: "SPONSOR A COMPREHENSIVE SCREENING CAMP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "12", label: "$12", description: "SPONSOR AN ORAL CANCER SCREENING KIT" },
-    { value: "30", label: "$30", description: "SUPPORT AN AWARENESS SESSION" },
-    { value: "60", label: "$60", description: "SUPPORT A SMALL SCREENING CAMP" },
-    { value: "120", label: "$120", description: "SPONSOR A COMPREHENSIVE SCREENING CAMP" },
-];
-
 export default function OralScanDonationForm() {
     return (
         <DonationForm
             cause="OralScan"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="1000"
-            defaultNonIndianAmount="12"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT ORALSCAN"
             formSubtitle="EARLY DETECTION SAVES LIVES"
+            hideAmount={true}
+            razorpayButtonId="pl_SRKz9206bzQ0iI"
         />
     );
 }
