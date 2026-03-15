d375ca4915e13a8bf81d9b454e0c4f5d852717b9
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 02:45:54 +0000

    In SOULCIRCLE donation FORM Only keep radio button for Indian and non-In

--- components/sections/donation-forms/SoulCircleDonationForm.tsx
+++ components/sections/donation-forms/SoulCircleDonationForm.tsx
@@ -1,31 +1,18 @@
-
 'use client';
 import DonationForm from "./DonationForm";
-
-const donationAmountsIndian = [
-    { value: "500", label: "₹500", description: "MAINTAIN APP FOR 1 MONTH" },
-    { value: "1000", label: "₹1000", description: "SUPPORT AI MODEL TRAINING" },
-    { value: "2500", label: "₹2500", description: "SUPPORT MULTILINGUAL CAPABILITIES" },
-    { value: "5000", label: "₹5000", description: "SUPPORT APP DEVELOPMENT" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "6", label: "$6", description: "MAINTAIN APP FOR 1 MONTH" },
-    { value: "12", label: "$12", description: "SUPPORT AI MODEL TRAINING" },
-    { value: "30", label: "$30", description: "SUPPORT MULTILINGUAL CAPABILITIES" },
-    { value: "60", label: "$60", description: "SUPPORT APP DEVELOPMENT" },
-];
 
 export default function SoulCircleDonationForm() {
     return (
         <DonationForm
             cause="SoulCircle"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="500"
-            defaultNonIndianAmount="6"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT SOULCIRCLE"
             formSubtitle="PROVIDE A SAFE SPACE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRLCra1orz60TY"
         />
     );
 }
