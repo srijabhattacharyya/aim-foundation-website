f79ac14f82fa443130da2efb233c3820a079d6ce
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 04:25:41 +0000

    In ROOTS OF CHANGE donation FORM Only keep radio button for Indian and n

--- components/sections/donation-forms/RootsOfChangeDonationForm.tsx
+++ components/sections/donation-forms/RootsOfChangeDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "1000", label: "₹1000", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "2500", label: "₹2500", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "5000", label: "₹5000", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "10000", label: "₹10000", description: "FUND AWARENESS WORKSHOPS" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "12", label: "$12", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "30", label: "$30", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "60", label: "$60", description: "FUND AWARENESS WORKSHOPS" },
-    { value: "120", label: "$120", description: "FUND AWARENESS WORKSHOPS" },
-];
-
 export default function RootsOfChangeDonationForm() {
     return (
         <DonationForm
             cause="Roots of Change"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="1000"
-            defaultNonIndianAmount="12"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT ROOTS OF CHANGE"
             formSubtitle="EMPOWER THROUGH EDUCATION"
+            hideAmount={true}
+            razorpayButtonId="pl_SRMtxbIuxDs3Yv"
         />
     );
 }
