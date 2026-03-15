3b1d1be70beca69c00c077889fa56c51143eb573
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 02:50:54 +0000

    In General Donation to support Gender equality donation FORM Only keep r

--- components/sections/donation-forms/GenderEqualityDonationForm.tsx
+++ components/sections/donation-forms/GenderEqualityDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "2500", label: "₹2500", description: "SUPPORT A VOCATIONAL TRAINING WORKSHOP" },
-    { value: "5000", label: "₹5000", description: "PROVIDE DIGITAL LITERACY FOR 2 WOMEN" },
-    { value: "10000", label: "₹10000", description: "SUPPORT A WOMEN'S HEALTH CAMP" },
-    { value: "25000", label: "₹25000", description: "SPONSOR A SELF-HELP GROUP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "30", label: "$30", description: "SUPPORT A VOCATIONAL TRAINING WORKSHOP" },
-    { value: "60", label: "$60", description: "PROVIDE DIGITAL LITERACY FOR 2 WOMEN" },
-    { value: "120", label: "$120", description: "SUPPORT A WOMEN'S HEALTH CAMP" },
-    { value: "300", label: "$300", description: "SPONSOR A SELF-HELP GROUP" },
-];
-
 export default function GenderEqualityDonationForm() {
     return (
         <DonationForm
             cause="Gender Equality"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="2500"
-            defaultNonIndianAmount="30"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT GENDER EQUALITY"
             formSubtitle="MAKE A DIFFERENCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRLITHUjakzVWU"
         />
     );
 }
