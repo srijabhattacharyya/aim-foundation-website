8e409bcd20fc8f3648280b2cbc3896c12c4e1c92
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 02:22:19 +0000

    In DETECT donation FORM Only keep radio button for Indian and non-Indian

--- components/sections/donation-forms/DetectDonationForm.tsx
+++ components/sections/donation-forms/DetectDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "1500", label: "₹1500", description: "SPONSOR SCREENING FOR 1 WOMAN" },
-    { value: "3000", label: "₹3000", description: "SPONSOR SCREENING FOR 2 WOMEN" },
-    { value: "7500", label: "₹7500", description: "SPONSOR SCREENING FOR 5 WOMEN" },
-    { value: "30000", label: "₹30000", description: "SPONSOR AN AWARENESS CAMP" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "18", label: "$18", description: "SPONSOR SCREENING FOR 1 WOMAN" },
-    { value: "36", label: "$36", description: "SPONSOR SCREENING FOR 2 WOMEN" },
-    { value: "90", label: "$90", description: "SPONSOR SCREENING FOR 5 WOMEN" },
-    { value: "360", label: "$360", description: "SPONSOR AN AWARENESS CAMP" },
-];
-
 export default function DetectDonationForm() {
     return (
         <DonationForm
             cause="Detect"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="1500"
-            defaultNonIndianAmount="18"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT DETECT"
             formSubtitle="MAKE A DIFFERENCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRKn2GcPutDzOA"
         />
     );
 }
