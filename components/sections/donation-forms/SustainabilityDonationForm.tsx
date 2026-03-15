f0e7351797358818558e480ff3685ab253bfdde8
Saibal Ganguly <tech@aimindia.org.in>
2026-03-15 03:57:26 +0000

    In General Donation to support Sustainability donation FORM Only keep ra

--- components/sections/donation-forms/SustainabilityDonationForm.tsx
+++ components/sections/donation-forms/SustainabilityDonationForm.tsx
@@ -2,30 +2,18 @@
 'use client';
 import DonationForm from "./DonationForm";
 
-const donationAmountsIndian = [
-    { value: "2000", label: "₹2000", description: "PLANT 20 TREES" },
-    { value: "5000", label: "₹5000", description: "SUPPORT A CLEANUP DRIVE" },
-    { value: "10000", label: "₹10000", description: "FUND AN ENVIRONMENTAL WORKSHOP" },
-    { value: "25000", label: "₹25000", description: "SPONSOR A RESTORATION PROJECT" },
-];
-
-const donationAmountsNonIndian = [
-    { value: "25", label: "$25", description: "PLANT 20 TREES" },
-    { value: "60", label: "$60", description: "SUPPORT A CLEANUP DRIVE" },
-    { value: "120", label: "$120", description: "FUND AN ENVIRONMENTAL WORKSHOP" },
-    { value: "300", label: "$300", description: "SPONSOR A RESTORATION PROJECT" },
-];
-
 export default function SustainabilityDonationForm() {
     return (
         <DonationForm
             cause="Sustainability"
-            donationAmountsIndian={donationAmountsIndian}
-            donationAmountsNonIndian={donationAmountsNonIndian}
-            defaultIndianAmount="2000"
-            defaultNonIndianAmount="25"
+            donationAmountsIndian={[]}
+            donationAmountsNonIndian={[]}
+            defaultIndianAmount=""
+            defaultNonIndianAmount=""
             formTitle="SUPPORT SUSTAINABILITY INITIATIVES"
-            formSubtitle="NURTURE OUR PLANET"
+            formSubtitle="MAKE A DIFFERENCE"
+            hideAmount={true}
+            razorpayButtonId="pl_SRMPzkM6TncNHB"
         />
     );
 }
